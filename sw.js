9e3699a79bdcae27622ab38bbfb6e4279f20f17c
simplelab472 <syouji226@gmail.com>
2026-09-23 07:34:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "bread-log-pwa-v32";
+const CACHE_NAME = "bread-log-pwa-v31";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=32",
-  "./app.js?v=32",
-  "./manifest.webmanifest?v=32",
+  "./styles.css?v=31",
+  "./app.js?v=31",
+  "./manifest.webmanifest?v=31",
   "./icon-192.png",
   "./icon-512.png"
 ];
